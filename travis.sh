a483be9b780c5f991236a3b5655055c869b7bacb
W. Duncan Fraser <duncan@wduncanfraser.com>
2016-01-31 18:54:51 -0900

    Removing coveralls support for now. Will most likely need to add back in with coveralls-cmake.

--- travis.sh
+++ travis.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 
-TRAVIS_OS_NAME=$1
-CXX=$2
-
 # Make bin directory and cd
 mkdir build || true
 cd build
 
 # Build project
 cmake ..
+make
 
 # Run Tests
-if [ "$CXX" = "g++-5" ] && [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    ./configure --enable-gcov && make && make check
-else
-    make
-fi
-
 ./neuralnet_tests
 ./goboard_tests
