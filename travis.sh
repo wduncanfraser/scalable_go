409630fceadfc894694dedbd4d9bdcc974ba1f13
W. Duncan Fraser <duncan@wduncanfraser.com>
2016-01-31 17:45:31 -0900

    Setting up coveralls supports.

--- travis.sh
+++ travis.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+TRAVIS_OS_NAME=$1
+CXX=$2
+
 # Make bin directory and cd
 mkdir build || true
 cd build
@@ -8,6 +11,11 @@ cd build
 cmake ..
 
 # Run Tests
-make
+if [ "$CXX" = "g++" ] && [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    ./configure --enable-gcov && make && make check
+else
+    make
+fi
+
 ./neuralnet_tests
 ./goboard_tests
