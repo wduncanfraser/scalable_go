2d1622753efb34f8cb7ce91a9dfdd4b28f2e792a
W. Duncan Fraser <duncan@wduncanfraser.com>
2016-02-29 19:33:21 -0900

    Implementing AB Prune and related functions/classes.

---
+++ ab_prune/ab_prune.h
@@ -0,0 +1,47 @@
+// Copyright [2016] <duncan@wduncanfraser.com>
+// Prototypes for Scalable Go AB prune algorithm and helper classes/functions
+
+#ifndef AB_PRUNE_AB_PRUNE_H_
+#define AB_PRUNE_AB_PRUNE_H_
+
+#include <vector>
+#include <cstdint>
+
+#include "neuralnet.h"
+#include "goboard.h"
+
+#define SEGMENT_MIN 3
+#define SEGMENT_MAX 7
+#define SEGMENT_DIVISION 2
+
+// ABPrune exceptions
+class ABPruneSegmentError : public std::runtime_error {
+ public:
+    ABPruneSegmentError() : std::runtime_error("ABPruneSegmentError") { }
+};
+
+// Function that returns a vector of all segment sizes for a board of specified size.
+// The specified size must be a valid segment size.
+std::vector<uint8_t> get_board_segments(const uint8_t board_size);
+
+// Function for taking the current board state of an instance of GoBoard and serializing it for ab_prune consumption.
+// Blank spaces return as 0, friendly pieces return as 1, enemy pieces return as -1
+std::vector<std::vector<double>> scalable_go_network_translation(const GoBoard &i_board, const bool color);
+
+// Alpha Beta Pruning algorithm for Go move generation
+double scalable_go_ab_prune(NeuralNet &network, const GoBoard &i_board, const int depth, double alpha, double beta,
+                            const bool move_color, const bool max_player, const bool player_color);
+
+
+// Class for holding a GoBoard neuralnet. Wrapper around NeuralNet
+class GoBoardNeuralNet {
+ private:
+    // Vector holding first layer of neural nets
+    std::vector<NeuralNet> layer1;
+
+    // Second layer neural net
+    NeuralNet layer2;
+ public:
+};
+
+#endif  // AB_PRUNE_AB_PRUNE_H_
